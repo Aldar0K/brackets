380b23f04cdc548ee1b2b6a82c392f25d670f7e9
Aldar <aldarkonov@mail.ru>
2022-02-20 14:24:25 +0300

    feat: finish js task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
+  const openBrackets = [];
+  const closeBrackets = [];
+  let arr = str.split('');
+  const result = [];
+
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (result[result.length - 1] === openBrackets[j] && arr[i] === closeBrackets[j]) {
+        result.pop();
+      } else {
+        if (arr[i] === openBrackets[j]) {
+          result.push(arr[i]);
+        }
+      }
+    }
+  }
   
+  if (result.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
